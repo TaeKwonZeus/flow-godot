c0b10da81dca5842986e1b8479e65286a0e9593c
TaeKwonZeus <dyatlovleonid@gmail.com>
2021-01-02 15:22:30 +0300

    Update player.gd

--- src/Scripts/Grid/Blocks/player.gd
+++ src/Scripts/Grid/Blocks/player.gd
@@ -7,19 +7,6 @@ func _process(delta):
 		_handle_movement()
 
 
-func _handle_input():
-	var vertical = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
-	var horizontal
-	
-	if vertical == 0:
-		horizontal = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	else:
-		horizontal = 0
-	
-	return Vector2(horizontal, vertical)
-
-
 func _handle_movement():
 	var direction = InputManager.movement_input() * SPEED_MODIFIER
 
