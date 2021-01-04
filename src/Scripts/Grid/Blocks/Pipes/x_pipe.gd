b85689b610da3e6106204216c87643bc7a2604da
TaeKwonZeus <dyatlovleonid@gmail.com>
2021-01-04 21:21:45 +0300

    Update x_pipe.gd

--- src/Scripts/Grid/Blocks/Pipes/x_pipe.gd
+++ src/Scripts/Grid/Blocks/Pipes/x_pipe.gd
@@ -3,11 +3,16 @@ extends Pipe
 
 
 func _get_outputs(input):
-	return [
-		-input,
-		input.rotated(deg2rad(90)).round(),
-		input.rotated(deg2rad(-90)).round(),
+	var output = [
+		Vector2.RIGHT * WORLD_MODIFIER,
+		Vector2.LEFT * WORLD_MODIFIER,
+		Vector2.UP * WORLD_MODIFIER,
+		Vector2.DOWN * WORLD_MODIFIER,
 	]
+	
+	output.erase(input)
+	
+	return output
 
 
 func _is_rotated_properly(_input):
